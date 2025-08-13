03cac1670b91996ae86bf3f14300efdefc9ead47
goku1312 <gokulmohan97@gmail.com>
2025-08-13 14:02:38 +0530

    ww

--- hairfix/app/urls.py
+++ hairfix/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings # new
+from  django.conf.urls.static import static #new
 urlpatterns = [
     path('',views.index,name='index'),
     path('about/',views.about,name='about'),
@@ -9,3 +10,8 @@ urlpatterns = [
     path('services/',views.services,name='services'),
  
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_URL)
